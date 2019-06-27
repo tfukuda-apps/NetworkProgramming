aa3532ec7c8abde0a6c7923c7b146d5a4334f186
tatasuki <i.am.ryuzyu@gmail.com>
2019-06-27 16:02:22 +0900

    [fix]character code

--- client.c
+++ client.c
@@ -9,43 +9,43 @@
 #define BUF_SIZE 256
 
 void DieWithError(char *errorMessage) {
-	perror(errorMessage);
-	exit(1);
+    perror(errorMessage);
+    exit(1);
 }
 
-void commun (int sock) {
-	char buf[BUF_SIZE];
-	int len_r;
-	char *message = “Hello server!”;
-	if (send(sock, message, strlen(message), 0) != strlen(message))
-		DieWithError(“send() sent a message of unexpected bytes”);
+void commun(int sock) {
+    char buf[BUF_SIZE];
+    int len_r;
+    char *message = "Hello server!";
 
-	if ((len_r = recv(sock, buf, BUF_SIZE, 0)) <= 0)
-		DieWithError(“recv() failed”);
-
-	buf[len_r] = ‘\0’;
-	printf( “%s\n”, buf );
+    if(send(sock, message, strlen(message), 0) != strlen(message))
+        DieWithError("send() sent a message of unexpected bytes");
+    
+    if ((len_r = recv(sock, buf, BUF_SIZE, 0)) <= 0)
+        DieWithError("recv() failed");
+    
+    buf[len_r] = '\0';
+    printf("%s\n", buf);
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 3)
-		DieWithError("the number of arguments is unexpected")
-	
-	char *server_ipaddr = argv[1];
-	int server_port = atoi(argv[2]);
-
-	int sock = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock  < 0) 
-		DieWithError(“socket () failed”);
-
-	struct sockaddr_in target;
-	target.sin_family = AF_INET;
-	target.sin_addr.s_addr = inet_addr(server_ipaddr);
-	target.sin_port = htons(server_port);
-	if (connect(sock, (struct sockaddr *)&target, sizeof(target)) < 0)
-		DieWithError(“connect() failed”);
-
-	commun(sock);
-	close(sock);
-	return 0;
+    if (argc != 3)
+        DieWithError("the number of arguments is unexpected");
+    
+    char *server_ipaddr = argv[1];
+    int server_port = atoi(argv[2]);
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        DieWithError("socket() failed");
+    
+    struct sockaddr_in target;
+    target.sin_family = AF_INET;
+    target.sin_addr.s_addr = inet_addr(server_ipaddr);
+    target.sin_port = htons(server_port);
+    if (connect(sock, (struct sockaddr*)&target, sizeof(target)) < 0)
+        DieWithError("connect() failed");
+    
+    commun(sock);
+    close(sock);
+    return 0;
 }
--- www/server.c
+++ www/server.c
@@ -4,82 +4,57 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 
 #define BUF_SIZE 256
 
 void DieWithError(char *errorMessage) {
-	perror(errorMessage);
-	exit(1);
+    perror(errorMessage);
+    exit(1);
 }
 
-void commun (int sock) {
-	char buf[BUF_SIZE];
-	int len_r;
+void commun(int sock) {
+    char buf[BUF_SIZE];
+    int len_r;
 	char *message = "お返しでーす";
 
-	if ((len_r = recv(sock, buf, BUF_SIZE, 0)) <= 0)
-		DieWithError(“recv() failed”);
-	buf[len_r] = ‘\0’;
-	printf( “%s\n”, buf );
-	
-	if (send(sock, message, strlen(message), 0) != strlen(message))
-		DieWithError(“send() sent a message of unexpected bytes”);
+    if ((len_r = recv(sock, buf, BUF_SIZE, 0)) <= 0)
+        DieWithError("recv() failed");
+    
+    buf[len_r] = '\0';
+    printf("%s\n", buf);
+
+    if(send(sock, message, strlen(message), 0) != strlen(message))
+        DieWithError("send() sent a message of unexpected bytes");
 }
 
-int main(int arg0, char **argv) {
-	char buf[BUF_SIZE];
-	int i;
-	
-	/***********************/
-	/*  クライアント情報の保存用  */
-	/***********************/
-	struct sockaddr_in clientAddress
-	unsinged int szClientAddr;
+int main(int argc, char *argv[]) {
+	struct sockaddr_in clientAddress;
+	unsigned int szClientAddr;
 	int cliSock;
 
-	/***********************/
-	/*  待ち受けソケットの作成  */
-	/***********************/
 	int servSock = socket(PF_INET, SOCK_STREAM, 0);
+    if (servSock < 0)
+        DieWithError("socket() failed");
+    
+    struct sockaddr_in servAddress;
+    servAddress.sin_family = AF_INET;
+    servAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddress.sin_port = htons(80);
 
-	/******************/
-	/*  マシン情報作成  */
-	/******************/
-	struct sockaddr_in servAddress;
+	bind(servSock, (struct sockaddr*)&servAddress, sizeof(servAddress));
 
-	servAddress.sin_family      = PF_INET;
-	servAddress.sin_port        = (in_port_t)htons(80);  // 待ち受けソケットのポート番号
-	servAddress.sin_addr.s_addr = htonl(INADDR_ANY);        // 待ち受けるための（自分の）IPアドレス（普段はINADDR_ANY）
-
-	/*********************************/
-	/* 待ち受けソケットにマシンの情報をセット  */
-	/*********************************/
-	bind(servSock, (struct sockaddr *)&servAddress, sizeof(servAddress));
-
-	/********************/
-	/*   待ち受け開始   */
-	/********************/
 	listen(servSock, 5);
 
-	while(1) {
+	while (1) {
 		szClientAddr = sizeof(clientAddress);
 		cliSock = accept(servSock, (struct sockaddr *)&clientAddress, &szClientAddr);
-		
-		/****************/
-		/* 　　通信開始    */
-		/****************/
-		
+
 		commun(cliSock);
 
 		close(cliSock);
 	}
 
-	/********************/
-	/* 待受用ソケットを */
-	/* 閉じる           */
-	/********************/
-	close(servSock);
-
-	return 0;
+    close(servSock);
+    return 0;
 }
