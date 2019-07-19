36ba253508fb9c87092d459d63eda38fc9862bf3
tfukuda-apps <49179329+tfukuda-apps@users.noreply.github.com>
2019-07-19 09:17:59 +0900

    [update]前の段階に戻した

--- www/server.c
+++ www/server.c
@@ -20,48 +20,21 @@ void commun(int sock) {
     char buf_old[BUF_SIZE];
     char buf2[2*BUF_SIZE];
     int len_r;
-	char response[BUF_SIZE];
+    char response[BUF_SIZE];
 
-    regex_t regBuf;
-    regmatch_t regMatch[1];	
-    const char *pattern = "GET[^\n]+HTTP";
-    char result[100];
-    char *uri;
-
-	buf_old[0] = '\0';
-    result[0] = '\0';
-
-    if (regcomp(&regBuf, pattern, REG_EXTENDED | REG_NEWLINE) != 0) {
-        DieWithError("regcomp failed.");
-    }
-
+    buf_old[0] = '\0';
+    
     while((len_r = recv(sock, buf, BUF_SIZE, 0)) > 0){
         buf[len_r] = '\0';
         sprintf(buf2, "%s%s", buf_old, buf);
 
         printf("%s", buf);
 
-        if ( regexec(&regBuf, buf2, 1, regMatch, 0) != 0 ){
-	        int startIndex = regMatch[0].rm_so;
-	        int endIndex = regMatch[0].rm_eo;
-	        strncpy(result, buf2 + startIndex, endIndex - startIndex);
-        }
-
-
         if (strstr(buf2, "\r\n\r\n")) {
             break;
         }
 		
-		sprintf(buf_old, "%s", buf);
-    }
-    regfree(&regBuf);
-
-    if (result[0] != '\0') {
-        uri = strtok(uri, " ");
-        uri = strtok(NULL, " ");
-        printf("%s\n", uri);
-    } else {
-        DieWithError("No URI.");
+	sprintf(buf_old, "%s", buf);
     }
 
     if (len_r <= 0)
