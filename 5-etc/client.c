e849d9a8ea55ea7d9464fdba2322c74b124bc316
tfukuda-apps <tfukuda@apps.kct.ac.jp>
2020-01-15 17:39:13 +0900

    [update]add Makefile

--- 5-etc/client.c
+++ 5-etc/client.c
@@ -5,12 +5,6 @@
 #include "np_common.h"
 
 #define BUF_SIZE 256
-#define MONEY_DIGIT_SIZE 10
-
-struct money{
-    int deposit;
-    int withdraw;
-};
 
 int main(int argc, char *argv[]) {
     // 実行時にはサーバのIPアドレスとポートを添えなければならない
@@ -30,54 +24,4 @@ int main(int argc, char *argv[]) {
 }
 
 void commun(int sock) {
-    char cmd[2] = "";                       // コマンド入力用
-    struct money msgMoney;                  // 引き出し額/預け入れ額
-    char money[BUF_SIZE];                   // 金額入力用
-    int result;                             // 結果
-    
-    printf("0:引き出し　1:預け入れ　2:残高照会　9:終了\n");
-    printf("何をしますか？ > ");
-    my_scanf(cmd, 1);
-    
-    // 入力値によって処理を分岐
-    switch (cmd[0]){
-        case '0':
-            // 引き出し処理
-            printf("引き出す金額を入力してください > ");
-            my_scanf(money, MONEY_DIGIT_SIZE);
-            msgMoney.deposit = htonl(0);
-            msgMoney.withdraw = htonl(atoi(money));
-            break;
-        case '1':
-            //預け入れ処理
-            printf("預け入れる金額を入力してください > ");
-            my_scanf(money, MONEY_DIGIT_SIZE);
-            msgMoney.deposit = htonl(atoi(money));
-            msgMoney.withdraw = htonl(0);
-            break;
-        case '2':
-            // 残高照会
-            msgMoney.deposit = htonl(0);
-            msgMoney.withdraw = htonl(0);
-            break;
-        default:
-            // 終了
-            printf("番号が確認できませんでした。\n");
-            return;
-    }
-
-    printf("%lu バイト\n", sizeof(msgMoney));
-
-    // 送信処理
-    if(send(sock, &msgMoney, sizeof(msgMoney), 0) != sizeof(msgMoney))
-        DieWithError("send() sent a message of unexpected bytes");
-
-    // 受信処理
-    read_certain_bytes(sock, &result, (int)sizeof(int));
-
-    // ネットワークバイトオーダから変換
-    result = ntohl(result);
-
-    // 表示処理
-    printf("残高は%d円になりました\n", result);
 }
--- 5-etc/server.c
+++ 5-etc/server.c
@@ -7,12 +7,6 @@
 
 
 #define BUF_SIZE 256
-#define MONEY_DIGIT_SIZE 10
-
-struct money{
-    int deposit;
-    int withdraw;
-};
 
 int main(int argc, char *argv[]) {
 	// クライアントの情報を格納するための変数
@@ -22,6 +16,10 @@ int main(int argc, char *argv[]) {
 	int cliSock;
 	// 子プロセスカウンタ
 	int numChildProc = 0;
+
+	// 実行時には待受ソケットに用いるポート番号を添えなければならない
+	if (argc != 2)
+		DieWithError("usage: ./server port");
 
 	// 待ち受け用ソケット
 	int servSock = prepare_server_socket(10001);
@@ -43,14 +41,12 @@ int main(int argc, char *argv[]) {
 			DieWithError("fork()に失敗");
 		} else if (pid == 0) {
 			// 子プロセス->通信して終了
-			printf("子\n");
 			close(servSock);
 			commun(cliSock);
 			close(cliSock);
 			exit(0);
 		} else{
 			// 親プロセス->次の待受の処理に移動
-			printf("親\n");
 			close(cliSock);
 
 			// 子プロセスカウンターをインクリメント
@@ -76,36 +72,5 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-
-// 本来はデータベースから現在の預金残高を取得
-int get_current_balance(){
-	return 1000000;
-}
-
-// 本来は預金残高をデータベースに登録
-void set_current_balance(int new_balance) {
-	return;
-}
-
 void commun(int sock) {
-	int balance = get_current_balance(); 	// 預金残高
-	struct money msgMoney;					// 受信した金額
-
-	// 引き出し/預け入れの金額を受信
-    read_certain_bytes(sock, &msgMoney, (int)sizeof(msgMoney));
-	msgMoney.deposit = ntohl(msgMoney.deposit);
-	msgMoney.withdraw = ntohl(msgMoney.withdraw);
-	
-	balance += msgMoney.deposit;
-	balance -= msgMoney.withdraw;
-
-	// データベースの預金残高を更新
-	set_current_balance(balance);
-
-	// ネットワークバイトオーダへ変換
-	balance = htonl(balance);
-
-	// クライアントへ残高を送信
-	if(send(sock, &balance, sizeof(balance), 0) != sizeof(balance))
-		DieWithError("send() sent a message of unexpected bytes");
 }
